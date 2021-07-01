fba30e58b5f83bedca7ecd2445fb7d0c645b7f58
Carl Parker <carljparker@fb.com>
2021-06-30 20:51:18 -0700

    Draft the composite chart

--- Parker_Carl_HW00.R
+++ Parker_Carl_HW00.R
@@ -248,5 +248,61 @@ dev.off()
 lm( csv.data.df$opioid.and.stim ~ csv.data.df$year )
 
 
+viz.oscomp.deaths.large <- function( out.name ) {
+  #
+  # Larger version of plot
+  #
+  png( out.name, 
+       width = 1200, height = 900, 
+       units = "px", pointsize = 16, 
+       bg = rgb( 248, 245, 236, maxColorValue = 255 ) 
+  )
+
+  par( 
+      cex.main = 3, cex.axis = 2.50, cex.lab = 3,
+      # margins: bottom, left, top and right
+      par( mar = c( 10, 11, 8, 8 ) + 0.1 )  
+  )
+
+  plot( 
+       csv.data.df$year, csv.data.df$stim.only,
+       ylim = c( 30, 175 ),
+       main = "Opioid-only and stim-only deaths by year",
+       xlab="", ylab = "",
+       pch = 24, cex=3, col="blue", bg="red", lwd=2
+  )
+
+  title( xlab="Year", cex.lab = 3, line = 5 )
+  title( ylab="Number of deaths", cex.lab = 3, line = 5 )
+
+}
+
+#
+# Add the stim-only trend line
+#
+viz.oscomp.deaths.large( "viz/opioid-stim-compare-deaths-trend-large.png" )
+                         
+abline( lm( csv.data.df$stim.only ~ csv.data.df$year ), lwd = 3, col = "slategrey" )
+
+points( 
+        x = 2019,
+        y = csv.data.df$opioid.only[ csv.data.df$year == 2019 ],
+        pch = 24, cex=3, col="black", bg="orange", lwd=2
+)
+                         
+#
+# Add the opioid-only trend line
+#
+abline( lm( csv.data.df$ opioid.only ~ csv.data.df$year ), lwd = 3, col = "slategrey" )
+                         
+dev.off()
+
+
+#
+# Get the intercept and slope
+#
+lm( csv.data.df$stim.only ~ csv.data.df$year )
+
+
 # --- END --- #
 
